ad134a0395688dbe824aae2c155fac3b4bbcb56d
Kristina Vujicic <xtinakg.93@gmail.com>
2022-06-23 00:05:37 +0200

    Admin Cities Testovi

--- IT_bootcamp_final_project/src/main/java/Pages/CitiesPage.java
+++ IT_bootcamp_final_project/src/main/java/Pages/CitiesPage.java
@@ -1,4 +1,103 @@
 package Pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class CitiesPage {
+
+    private WebDriver driver;
+
+    public CitiesPage (WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public WebElement getNewItemButton () {
+        return driver.findElement(By.xpath("// button [contains (@class, 'btnNewItem ')]"));
+    }
+
+    public void waitForEditiPopUpToBeVisible() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By
+                .xpath("// div [contains (@class, 'dlgNewEditItem ')]")));
+
+    }
+
+    public WebElement getNewItemInput(){
+        return driver.findElement(By.xpath("// input [@id= 'name']"));
+    }
+
+    public WebElement getSaveButton () {
+        return driver.findElement(By.xpath("//button[contains(@class, 'btnSave')]"));
+    }
+
+
+    public void waitForSuccessPopUpToBeVisible(){
+       WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+       wait.until(ExpectedConditions.visibilityOfElementLocated(By
+               .xpath("//div[contains(text(),'Saved successfully')]")));
+    }
+
+    public void waitForDeletePopUpToBeVisible(){
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By
+                .xpath("//div[contains(text(),' Deleted successfully ')]")));
+    }
+
+    public WebElement getMessageTextFromSuccesPopUp() {
+        return driver.findElement(By.xpath("// div [contains (@class, 'success')]"));
+    }
+
+    public WebElement getSearchInout () {
+
+        return driver.findElement(By.id("search"));
+    }
+
+    public void waitForNumbersOfRowsToBe (int rowIndex) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.numberOfElementsToBe(By.tagName("tr"), rowIndex +1 ));
+    }
+
+    public WebElement getTableCellFromRow (int rowIndex, int cellIndex){
+        return driver
+                .findElement(By.xpath("//table/tbody/tr[" + rowIndex + "]/td[" + cellIndex + "]"));
+    }
+
+    public WebElement getEditButtonFromRow (int row){
+        return driver.findElement(By.xpath("//tbody/tr[" + row + "]/td/div/button[@id = 'edit']"));
+    }
+
+    public WebElement getDeleteButtonFromRow (int row){
+        return driver.findElement(By.xpath("//tbody/tr[" + row + "]/td/div/button[@id = 'delete']"));
+    }
+
+    public void waitForDeletePopUpToBeVisable(){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions
+                .visibilityOfElementLocated(
+                        By.xpath("//*[text() = 'Do you really want to delete this item?']")));
+    }
+
+    public WebElement getDeleteButtonInDeletePopUp(){
+        return driver.findElement(By.xpath("//span[contains(text(), ' Delete')]"));
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- IT_bootcamp_final_project/src/main/java/Tests/AdminCitiesTests.java
+++ IT_bootcamp_final_project/src/main/java/Tests/AdminCitiesTests.java
@@ -1,4 +1,144 @@
 package Tests;
 
-public class AdminCitiesTests {
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AdminCitiesTests extends BasicTest {
+
+
+    @Test (priority = 1)
+    public void visitsTheAdminCitiesPageAndListCities () throws InterruptedException {
+
+        navPage.getLoginButton().click();
+        loginPage.getEmail().click();
+        loginPage.getEmail().sendKeys("admin@admin.com");
+
+        loginPage.getPassword().click();
+        loginPage.getPassword().sendKeys("12345");
+
+        loginPage.getLogInButtonOnLogInPage().click();
+
+        navPage.getAdminButton().click();
+        Thread.sleep(1000);
+
+        navPage.getCityButton().click();
+
+        Assert.assertTrue(driver.getCurrentUrl().contains("/admin/cities"),
+                "[ERROR] Page url does not contain /admin/cities");
+
+    }
+
+    @Test (priority = 2 )
+    public void checksInputTypesForCreateOrEditNewCity ()  {
+
+
+        navPage.getAdminButton().click();
+
+        navPage.getCityButton().click();
+
+        citiesPage.getNewItemButton().click();
+
+        citiesPage.waitForEditiPopUpToBeVisible();
+
+        Assert.assertEquals(citiesPage.getNewItemInput().getAttribute("type"),
+                "text",
+                "[ERROR] New item input 'type' attribute is not 'text'");
+
+    }
+
+    @Test (priority = 3)
+    public void createNewCity () {
+
+        navPage.getAdminButton().click();
+        navPage.getCityButton().click();
+
+        citiesPage.getNewItemButton().click();
+        citiesPage.waitForEditiPopUpToBeVisible();
+
+        citiesPage.getNewItemInput().sendKeys("KrisVuj's");
+        citiesPage.getSaveButton().click();
+
+        citiesPage.waitForSuccessPopUpToBeVisible();
+
+        Assert.assertTrue(citiesPage.getMessageTextFromSuccesPopUp().getText().contains("Saved successfully"));
+
+    }
+
+    @Test (priority = 4)
+    public void editCity () {
+
+        navPage.getAdminButton().click();
+        navPage.getCityButton().click();
+
+        citiesPage.getSearchInout().sendKeys("KrisVuj's");
+        citiesPage.waitForNumbersOfRowsToBe(1);
+        citiesPage.getEditButtonFromRow(1).click();
+        citiesPage.getNewItemInput().click();
+
+        new Actions(driver)
+                .keyDown(Keys.CONTROL)
+                .sendKeys("a")
+                .sendKeys(Keys.BACK_SPACE)
+                .perform();
+
+        citiesPage.getNewItemInput().sendKeys("");
+        citiesPage.getNewItemInput().sendKeys("VujKris's");
+        citiesPage.getSaveButton().click();
+
+
+
+        citiesPage.waitForSuccessPopUpToBeVisible();
+        Assert.assertTrue(citiesPage.getMessageTextFromSuccesPopUp().getText().contains("Saved successfully"),
+                "[ERROR] The message from pop-up should be 'Saved successfully'" );
+
+    }
+
+    @Test (priority = 5)
+    public void searchCity () {
+
+        navPage.getAdminButton().click();
+        navPage.getCityButton().click();
+        citiesPage.getSearchInout().sendKeys("VujKris's");
+        citiesPage.waitForNumbersOfRowsToBe(1);
+
+        Assert.assertTrue(citiesPage.getTableCellFromRow(1,2).getText().contains("VujKris's"),
+                "[ERROR] The 'Name' column does not contain expected result");
+
+    }
+
+    @Test (priority = 6)
+    public void deleteCity () {
+
+        navPage.getAdminButton().click();
+        navPage.getCityButton().click();
+
+        citiesPage.getSearchInout().sendKeys("VujKris's");
+        citiesPage.waitForNumbersOfRowsToBe(1);
+
+        citiesPage.getDeleteButtonFromRow(1).click();
+        citiesPage.waitForDeletePopUpToBeVisable();
+        citiesPage.getDeleteButtonInDeletePopUp().click();
+
+        citiesPage.waitForDeletePopUpToBeVisible();
+
+        messagePopUpPage.waitForPopUpDeleteCityToBeVisible();
+
+        Assert.assertTrue(messagePopUpPage.getElementsWithVisibleMessagesForCities().getText().contains("Deleted"),
+                "[ERROR] City is not deleted");
+    }
+
+
+
+
+
+
+
+
+
+
 }
+
+
+
